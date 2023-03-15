243ab5f300b0bbb01d09b8ebe5f4f70ef49665cb
kumikomi21 <blueskyjob11@gmail.com>
2023-03-15 13:24:10 +0900

    途中経過、枠組作成まで。コーディングはまだです。

--- Brain/Brain.ino
+++ Brain/Brain.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "Brain.h"
+#include "DicisionButton.h"
+#include "AnotherTerminal.h"
 
+//ToDo 後でボタン確認
+DicisionButton btn = DicisionButton(int BTN_PIN1, BTN_MODE D_READ); /* 時間 */
+
+
+void setup() {
+  //電池を入れると電源が入りBluetooth接続
+  //read()でメインのBluetoothからデータを取得する
+  //Serial.begin()で子機のBluetoothとの通信速度(データ通信レート)を決める
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  //ボタンが押されたかを検知？
+  
 }
