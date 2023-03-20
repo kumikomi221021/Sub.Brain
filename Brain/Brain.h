14ab5ad1d2492a9e6e74a6d3b1c9e405175eeb70
kumikomi21 <blueskyjob11@gmail.com>
2023-03-20 10:54:56 +0900

    金城さんコード書き換え前

--- Brain/Brain.h
+++ Brain/Brain.h
@@ -1,1 +1,13 @@
+#pragma once
 
+
+#define esp_read_mac();      //ESPアドレス
+ - uint8_t macBT[6];  //ESPアドレス
+ 
+  public:
+ int sabButton:
+ Serial.println();
+ Serial.begin();
+ Serial.write();
+ Serial.read()
+}
